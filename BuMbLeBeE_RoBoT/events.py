615db86fcc974b1e77de18f7597e8e09aeae5bca
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2022-02-26 08:51:35 +0530

    Create events.py

---
+++ BuMbLeBeE_RoBoT/events.py
@@ -0,0 +1,59 @@
+from BuMbLeBeE_RoBoT import tbot, ubot, OWNER_ID
+import os, glob, re, time, logging, sys
+from pathlib import Path
+from telethon import events
+
+def BuMbLeBeE_RoBoT(**args):
+   pattern = args.get('pattern', None)
+   r_pattern = r'^[*"]'
+   if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+   args['pattern'] = pattern.replace('^/', r_pattern, 1)
+   def decorator(func):
+        async def wrapper(check):
+          if not check.sender_id == int(OWNER_ID):
+            return
+          try:
+                await func(check)
+          except BaseException:
+                return
+        ubot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+   return decorator   
+  
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import BuMbLeBeE_RoBoT.events
+
+        path = Path(f"BuMbLeBeE_RoBoT/modules/{shortname}.py")
+        name = "BuMbLeBeE_RoBoT.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Successfully imported " + shortname)
+    else:
+        import importlib
+        import BuMbLeBeE_RoBoT.events
+
+        path = Path(f"BuMbLeBeE_RoBoT/modules/{shortname}.py")
+        name = "BuMbLeBeE_RoBoT.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.ubot = ubot
+        mod.tbot = tbot
+        mod.logger = logging.getLogger(shortname)
+        spec.loader.exec_module(mod)
+        sys.modules["BuMbLeBeE_RoBoT.modules." + shortname] = mod
+        print("Successfully imported " + shortname)
+            
+          
+path = "BuMbLeBeE_RoBoT/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", "")) 
