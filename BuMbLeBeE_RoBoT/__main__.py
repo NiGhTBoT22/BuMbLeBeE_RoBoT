d32dbe713c0f3d269ddb517dd6087c6e4f816f2b
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2022-02-26 08:31:12 +0530

    Update __main__.py

--- BuMbLeBeE_RoBoT/__main__.py
+++ BuMbLeBeE_RoBoT/__main__.py
@@ -1,1 +1,14 @@
+from BuMbLeBeE_RoBoT import ubot, tbot, TOKEN
+import BuMbLeBeE_RoBoT.events
+import sys
+
+try:
+  ubot.start()
+  tbot.start(bot_token=TOKEN)
+except:
+  print("Invalid STRING/Token__bot exiting...")
+  sys.exit()
+  
+ubot.run_until_disconnected()
+tbot.run_until_disconnected()
 
